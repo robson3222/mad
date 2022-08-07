db693980b2a1cc1c37c1ba9dbf81e39974272212
robson3222 <robson3222@gmail.com>
2022-08-07 19:54:23 -0300

    first commit

---
+++ empresas/models.py
@@ -0,0 +1,21 @@
+from datetime import timezone
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    local_arm = models.CharField(max_length=10)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    dat = models.CharField(max_length=10)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
